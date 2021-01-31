4bba33b1886dd3db5fbae7cd12ece15274bdfd94
Joe <jparkkennaby@gmail.com>
2021-01-31 14:08:25 +0000

    import bootstrap scss into index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from 'src/App';
 import reportWebVitals from './reportWebVitals';
+import "bootstrap/scss/bootstrap.scss";
 
 ReactDOM.render(
   <React.StrictMode>
