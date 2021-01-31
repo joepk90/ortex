f3f09c4eadfdb0e49fa495b241314ed6862002e8
Joe <jparkkennaby@gmail.com>
2021-01-31 18:46:18 +0000

    conver Login component into class component. setup Popup and ResetPasswordComponent. setup event to close popup on form submission

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,25 +1,86 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import LoginForm from 'src/components/LoginForm';
+import ResetPasswordForm from 'src/components/ResetPasswordForm';
 import Container from 'src/components/common/container';
 import Section from 'src/components/common/section';
+import Popup from 'reactjs-popup';
+import 'reactjs-popup/dist/index.css';
 
-const Login = () => {
-    return (
-        <main className="login">
+class Login extends Component {
 
-            <Section>
-                <Container>
+    state = {
+        popupOpen: null
+    }
 
-                    <h1 className="text-center">Login</h1>
+    componentDidUpdate(prevProps, prevState) {
 
-                    <div className="container-narrow mx-auto">
-                        <LoginForm />
-                    </div>
+        const popupOpenPrev = prevState.popupOpen;
+        const popupOpenCurrent = this.state.popupOpen;
 
-                </Container>
-            </Section>
+        if (popupOpenCurrent === popupOpenPrev && popupOpenPrev === false) {
+            this.setState({
+                popupOpen: null
+            })
+        }
+    }
 
-        </main>
-    );
+    handlePopupOpen() {
+
+        const { popupOpen } = this.state;
+
+        if (popupOpen === null) return null;
+
+        return false;
+
+    }
+
+    handleResetPasswordSubmit() {
+
+        this.setState({
+            popupOpen: false
+        })
+    }
+
+    render() {
+        return (
+            <main className="login">
+
+                <Section>
+                    <Container>
+
+                        <h1 className="text-center">Login</h1>
+
+                        <div className="container-narrow mx-auto">
+                            <LoginForm />
+                        </div>
+
+                        <div className="container-narrow mx-auto d-flex justify-content-center py-5">
+                            <Popup
+                                open={this.handlePopupOpen()}
+                                trigger={<Link to='#'>Reset Password</Link>}
+                                modal={true}
+                            >
+
+                                <div className="container-narrow mx-auto p-5">
+
+                                    <h5 className="text-center pb-4">Enter your email to reset your password</h5>
+
+                                    <ResetPasswordForm
+                                        onSubmit={() => this.handleResetPasswordSubmit()}
+                                    />
+                                </div>
+
+                            </Popup>
+                        </div>
+
+                    </Container>
+                </Section>
+
+            </main>
+        );
+    }
 }
 
 export default Login;
+
