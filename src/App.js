bc10541aa0ece59d764d23f3f35c7017508a973f
Joe <jparkkennaby@gmail.com>
2021-01-31 16:58:38 +0000

    fix import and className errors in App and Navigation component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Route, Switch, Redirect } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import Header from 'src/components/Header';
 import Login from 'src/components/Login';
 import Home from 'src/components/Home';
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -4,9 +4,9 @@ import { NavLink } from 'react-router-dom';
 class Navigation extends Component {
     render() {
         return (
-            <nav class="navbar navbar-expand-lg navbar-dark bg-black">
-                <ul class="navbar-nav">
-                    <li class="nav-item">
+            <nav className="navbar navbar-expand-lg navbar-dark bg-black">
+                <ul className="navbar-nav">
+                    <li className="nav-item">
                         <NavLink className="nav-link" to="/login">Log In</NavLink>
                     </li>
                 </ul>
