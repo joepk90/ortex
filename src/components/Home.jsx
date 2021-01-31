6cc7d9f9ca7e04f5dffd1977111d291af7e775a2
Joe <jparkkennaby@gmail.com>
2021-01-31 19:38:03 +0000

    setup and include LatestPrice comonent. update Home component to connet to the websocket after component mounts. update state and pass to the LatestPrice component (websocket not working - using hardcoded data)

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,18 +1,65 @@
+import React, { Component } from 'react';
+import Socket from 'simple-websocket';
 import Container from 'src/components/common/container';
 import Section from 'src/components/common/section';
+import LatestPrice from 'src/components/LatestPrice'
 
-const Home = () => {
-    return (
-        <div className="home">
+class Home extends Component {
 
-            <Section>
-                <Container>
-                    <h1 className="text-center">Home</h1>
-                </Container>
-            </Section>
+    // hardcoded state as websocket does not seem to be working
+    state = {
+        price: 1.2,
+        dt: '1612120749'
+    }
 
-        </div>
-    );
+    componentDidMount() {
+
+        var socket = new Socket('ws://stream.tradingeconomics.com/?client=guest:guest')
+
+        socket.on('connect', () => {
+            socket.send('"{"topic": "subscribe", "to": "EURUSD:CUR"}"')
+        })
+
+        socket.on('data', (data) => {
+
+            const { dt, price } = data;
+
+            if (!dt || !price) return;
+
+            this.setState({
+                price,
+                dt
+            })
+
+        })
+
+    }
+
+
+    render() {
+
+        const { price, dt } = this.state;
+
+        return (
+            <div className="home">
+
+                <Section>
+                    <Container>
+
+                        <div className="d-block mx-auto">
+                            <LatestPrice
+                                price={price}
+                                time={dt}
+                            />
+                        </div>
+
+                    </Container>
+                </Section>
+
+            </div>
+
+        );
+    }
 }
 
 export default Home;
---
+++ src/components/LatestPrice.jsx
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import moment from 'moment';
+
+class LatestPrice extends Component {
+
+    state = {}
+
+    renderTime() {
+        const { time } = this.props;
+
+        if (!time) return '';
+
+        const timeString = moment.unix(time).format('dddd, MMMM Do, YYYY h:mm:ss A')
+
+        if (!timeString) return '';
+
+        return (
+            <p className="latest-price__time">{timeString}</p>
+        )
+    }
+
+    renderPrice() {
+
+        const { price } = this.props;
+
+        if (!price) return '';
+
+        return (
+            <p className="latest-price__price">${price}</p>
+        )
+
+    }
+
+    render() {
+        return (
+            <div className="latest-price text-center">
+
+                {this.renderPrice()}
+                {this.renderTime()}
+
+            </div>
+        );
+    }
+}
+
+export default LatestPrice;
